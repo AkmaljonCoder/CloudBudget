e53defea2e780ea20a8c852020bdfba09e99aefe
Akmaljon Azimov <96201968+AkmaljonCoder@users.noreply.github.com>
2022-10-26 00:40:56 +0500

    akmal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {React, useEffect} from 'react'
+import AOS from 'aos'
+import './App.css'
+import "aos/dist/aos.css";
+
+// components
+
+  return (
+    <> 
+      <div>project</div>
+    </>
+  )
+}
+
+export default App
